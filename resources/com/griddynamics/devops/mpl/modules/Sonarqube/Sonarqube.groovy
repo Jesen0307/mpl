4a5ecd49c8bb357dcdecbb8c7b67196ee51fc785
Jesen0307 <jasontandiari@gmail.com>
2026-09-22 13:39:42 +0700

    Fix

--- resources/com/griddynamics/devops/mpl/modules/Sonarqube/Sonarqube.groovy
+++ resources/com/griddynamics/devops/mpl/modules/Sonarqube/Sonarqube.groovy
@@ -3,10 +3,10 @@
 
 if (fileExists('pom.xml')) {
     echo '[Sonarqube] Detected Maven project (pom.xml found). Routing to Sonarqube/Maven...'
-    MPLModule('Maven')
+    MPLModule('Sonarqube/Maven')
 } else if (fileExists('build.gradle') || fileExists('build.gradle.kts')) {
     echo '[Sonarqube] Detected Gradle project (build.gradle found). Routing to Sonarqube/Gradle...'
-    MPLModule('Gradle')
+    MPLModule('Sonarqube/Gradle')
 } else {
     error '[Sonarqube] Failed to detect build system! Neither pom.xml nor build.gradle/build.gradle.kts were found.'
 }
