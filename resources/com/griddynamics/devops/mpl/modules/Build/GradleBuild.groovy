b69d018d6ed8029b54a89073444692e55c86c67c
Jesen0307 <jasontandiari@gmail.com>
2026-09-25 11:52:18 +0700

    Adding  Gradle Build Config

--- resources/com/griddynamics/devops/mpl/modules/Build/GradleBuild.groovy
+++ resources/com/griddynamics/devops/mpl/modules/Build/GradleBuild.groovy
@@ -3,9 +3,9 @@ sh 'chmod +x gradlew || true'
 
 sh '''
   if [ -f "./gradlew" ]; then
-    ./gradlew --console=plain classes printRuntimeClasspath --no-daemon
+    ./gradlew --console=plain classes printRuntimeClasspath dependencies --write-locks --no-daemon
   else
-    gradle --console=plain classes printRuntimeClasspath --no-daemon
+    gradle --console=plain classes printRuntimeClasspath dependencies --write-locks --no-daemon
   fi
 '''
 
